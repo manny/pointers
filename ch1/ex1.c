e62e4498977d43932b34958addf27450599f6aa0
Manny <mjl288@scarletmail.rutgers.edu>
2013-09-05 01:42:39 -0400

    first commit

---
+++ ch1/ex1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	
+	printf("size of a short is %d\n" , sizeof(short));
+	printf("size of an int is %d\n" ,sizeof(int));
+	printf("size of an long is %d\n" ,sizeof(long));
+	return 0;
+
+}
